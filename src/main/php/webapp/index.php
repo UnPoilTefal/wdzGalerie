51213aee2c094e359e7d0d4e5f42a69f15e7f54b
UnPoilTefal <armefatale@gmail.com>
2012-10-30 00:42:36 +0100

    Reorg

--- src/main/php/webapp/index.php
+++ src/main/php/webapp/index.php
@@ -1,32 +1,77 @@
-<html>
-    <head>
-        <?php
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<?php require_once 'application/classes/init.php';?>
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+<title>Photo Gallery</title>
+<link href="css/ui-gallery/jquery-ui-1.9.0.custom.min.css" rel="stylesheet">
+<script src="http://code.jquery.com/jquery.js"></script>
+<script src="http://code.jquery.com/ui/1.9.0/jquery-ui.js"></script>
+<script>
+		$(function() {
+			$( "#tabs" ).tabs();
+		});
+	</script>
+</head>
+<body>
 
-            # BOOTSTRAP FILE
-            # SAMPLE by (c) banaalo - banaalo-mdeia.de; marco schulz
+<div id="tabs">
+	<ul>
+		<li><a href="#tabs-1">Affichage</a></li>
+		<li><a href="#tabs-2">Administration</a></li>
+	</ul>
+	<div id="tabs-1"><?=$dispContent->displayListGallery(); ?></div>
+	<div id="tabs-2">
+	
+	<?php 
 
-            include_once 'css/Css.php';
-            include_once 'javascript/JavaScript.php';
+if ($handle = opendir($configEnv->getFileUrl()))
+{
+	$contenu_galeries=array();
+	$cpt=0;
+	while (false !== ($file = readdir($handle)))
+	{
+		if($file != "." && $file != ".." ){
+			if(is_dir($configEnv->getFileUrl() . '/' .$file))
+				$contenu_galeries[$cpt]=$file;
+		}
+		$cpt++;
+	}
+	closedir($handle);
+}
+echo "<div><span>Initialisation des fichiers XML :</span></div><ul>";
+foreach ($contenu_galeries as $nom_gallery) {
 
-            echo css();
-            echo javascript();
+	echo "<li><a href='index.php?galleryname=" . $nom_gallery . "&action=refreshxml'>Initialisation de " . $nom_gallery . "</a></li>";
+	
 
-        ?>
-        <title>PHP MAVEN SAMPLE WEBAPP PAGE</title>
-    </head>
+}
+echo "</ul>";
+echo "<div><span>Organisation des images :</span></div><ul>";
+foreach ($contenu_galeries as $nom_gallery) {
 
-    <body>
+	echo "<li><a href='sort.php?galleryname=" . $nom_gallery . "'>Organisation de " . $nom_gallery . "</a></li>";
+	
 
-        <!-- insert code -->
-        <?php
+}
+echo "</ul>"
+?>
+<?php
+ if(isset($_REQUEST['action']) AND $_REQUEST['action'] == 'refreshxml') {
+	$galleryName = $_REQUEST['galleryname'];
+	$xmlGal = new XmlGallery();
+	echo "<div>" . $xmlGal->generateXml($galleryName) . "</div>";
+}	
+?>
+	
+	
+	
+	</div>
+</div>
 
-            require_once 'lib/org/phpmaven/library/LibraryClass.php';
-            
-            use org\phpmaven\library\LibraryClass as Library;
 
-            Library::getInstance();
-            echo Library::doFoo(true);
 
-        ?>
-    </body>
+
+	
+</body>
 </html>
